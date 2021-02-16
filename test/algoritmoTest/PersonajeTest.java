cb39ca3f01db4857e0c2cbd79e24c7492652826a
Ramaxx95 <57535047+Ramaxx95@users.noreply.github.com>
2021-02-16 15:23:35 -0300

    Update PersonajeTest.java

--- test/algoritmoTest/PersonajeTest.java
+++ test/algoritmoTest/PersonajeTest.java
@@ -1,12 +1,7 @@
 package algoritmoTest;
 
-import junit.framework.Assert;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import algoritmo.Personaje;
 

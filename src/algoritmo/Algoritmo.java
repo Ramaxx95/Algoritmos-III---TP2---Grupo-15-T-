59a5aac5026b19bf4660235583a041b53b0f73ac
Ramaxx95 <57535047+Ramaxx95@users.noreply.github.com>
2021-02-20 15:37:27 -0300

    Update Algoritmo.java

--- src/algoritmo/Algoritmo.java
+++ src/algoritmo/Algoritmo.java
@@ -16,6 +16,10 @@ public class Algoritmo {
     public void agregarBloque(Bloques un_bloque){
         this.listaAlgoritmo.add(un_bloque);
     }
+    
+    public void borrarBloques(){
+        this.listaAlgoritmo.clear();
+    }
 
     public void ejecutar(Personaje un_personaje, Dibujo un_dibujo){
         this.listaAlgoritmo.forEach(bloques -> bloques.ejecutarBloque(un_personaje, un_dibujo));

cc92b8cc1062f506d4ae869c92ef391fd1527fbd
Ramaxx95 <57535047+Ramaxx95@users.noreply.github.com>
2021-02-20 21:32:07 -0300

    Add files via upload

---
+++ src/excepciones/NoHayAlgoritmoGuardadoException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+/*Salta cuando se desea guardar un algortimo inicialmente vacio.*/
+
+public class NoHayAlgoritmoGuardadoException extends Exception{
+
+    public NoHayAlgoritmoGuardadoException(){
+    }
+}

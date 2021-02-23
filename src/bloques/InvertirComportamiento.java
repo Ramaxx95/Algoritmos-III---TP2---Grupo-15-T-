c429e168f72819c805b85c215e30f5040abca81b
Ramaxx95 <57535047+Ramaxx95@users.noreply.github.com>
2021-02-23 18:17:05 -0300

    Add files via upload

---
+++ src/bloques/InvertirComportamiento.java
@@ -0,0 +1,29 @@
+package bloques;
+
+import algoritmo.Personaje;
+import dibujos.Dibujo;
+
+import java.util.ArrayList;
+
+public class InvertirComportamiento implements Bloques{
+
+    private ArrayList<Bloques> bloques;
+
+    public InvertirComportamiento(){
+        this.bloques = new ArrayList<>();
+    }
+
+    public void agregarBloque(Bloques un_bloque){
+        this.bloques.add(un_bloque);
+    }
+
+    @Override
+    public void ejecutarBloque(Personaje un_personaje, Dibujo un_dibujo) {
+        this.bloques.forEach(bloque -> bloque.ejecutarBloqueInvertido(un_personaje, un_dibujo));
+    }
+
+    @Override
+    public void ejecutarBloqueInvertido(Personaje un_personaje, Dibujo un_dibujo) {
+        this.bloques.forEach(bloque -> bloque.ejecutarBloque(un_personaje, un_dibujo));
+    }
+}

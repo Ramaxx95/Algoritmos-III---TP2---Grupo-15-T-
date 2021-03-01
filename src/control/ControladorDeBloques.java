c6e135b0ed225602ace310f393bdb9f1594a473a
Ramaxx95 <57535047+Ramaxx95@users.noreply.github.com>
2021-03-01 18:18:23 -0300

    Se agrega InvertirComportamiento a lo visual (#8)
    * Se actualizan las imagenes
    
    * Delete repetir.png
    
    * Update ControladorDeBloques.java
    
    * Update CreadorDeBloquesVista.java
    
    * para crear bloques de invertirComportamiento

--- src/control/ControladorDeBloques.java
+++ src/control/ControladorDeBloques.java
@@ -10,9 +10,9 @@ public class ControladorDeBloques{
 	public static String moverIzquierda = "File:src/img/izquierda.png";
 	public static String subirLapiz = "File:src/img/subirLapiz.png";
 	public static String bajarLapiz = "File:src/img/bajarLapiz.png";
-	public static String repetirDosVeces = "File:src/img/repetir.png";
-	public static String repetirTresVeces = "File:src/img/repetir.png";
-	
+	public static String repetirDosVeces = "File:src/img/repetir_x2.png";
+	public static String repetirTresVeces = "File:src/img/repetir_x3.png";
+	public static String invertirComportamiento = "File:src/img/invertir.png";
 	
 	public ControladorDeBloques() {
 		
